7d017378d92d0451796badc4fe0b8f515f0b0b0d
Ryan <90472475+ryancamargo@users.noreply.github.com>
2024-07-18 11:14:11 -0300

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,42 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) throws IOException {
+
+		Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+			@Override
+			public void handle(WindowEvent arg0) {
+				
+				Platform.exit();
+				System.exit(0);
+				
+			}
+			
+		});
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
